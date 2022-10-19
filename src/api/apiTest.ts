9ab9fa2ff20d82e650991bf9329b2488c79ec045
rzh11111111 <294346639@qq.com>
2022-10-19 11:44:32 +0800

    feat: 11

--- src/api/apiTest.ts
+++ src/api/apiTest.ts
@@ -1,8 +1,11 @@
 import http from '@/utils/request'
-
+import env from '@/config/env'
 const apiTest = {
   getTest: (params: GetTest.params) => http.get<GetTest.data>('/test', params),
   postTest: (params: PostTest.params) => http.post<PostTest.data>('/test', params),
+  register: (params: PostTest.params) => http.post<PostTest.data>('/users/client/register', params),
+  createUpload: (params: GetTest.params) => http.get<GetTest.data>(env.onloadurl+'/admin/auth/system/oss/policy', params),
+  otherRegister: (params: PostTest.params) => http.post<PostTest.data>(env.onloadurl+"/users/client/register", params),
 }
 
 export default apiTest
--- src/components.d.ts
+++ src/components.d.ts
@@ -7,8 +7,13 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    CitySelect: typeof import('./components/city-select/city-select.vue')['default']
+    LabelInput: typeof import('./components/labelInput.vue')['default']
+    LiuyunoTabs: typeof import('./components/liuyuno-tabs/liuyuno-tabs.vue')['default']
     PullList: typeof import('./components/pullList.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Upload: typeof import('./components/upload/upload.vue')['default']
+    ZhangyuQrcodePoster: typeof import('./components/zhangyu-qrcode-poster/zhangyu-qrcode-poster.vue')['default']
   }
 }
