227d4295e4d7fe5d99b1c472cb34082384bc8cd5
rzh11111111 <294346639@qq.com>
2022-10-19 11:43:45 +0800

    feat: 11

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
 import env from '@/config/env'
-
+import apiTest from '@/api/apiTest'
+import user from '@/store/user'
 onLaunch(() => {
   console.log('App Launch')
   console.log(env.apiEnv, 'apiEnv')
   const appStore = useStore('app')
   const systemInfo = appStore.getSystemInfo()
   console.log(systemInfo, 'systemInfo')
+  const user_store = user()
+  const users = {
+    deviceUuid: systemInfo.deviceId,
+    deviceType: 'mini',
+    clientInfoType: 21,
+    versionNo: systemInfo.version
+  }
+    apiTest.otherRegister(users).then(res=>{
+      console.log(res,'otherRegister')
+      user_store.id = res.result.id
+    })
+
 })
 onShow(() => {
   console.log('App Show')
@@ -14,7 +27,10 @@ onShow(() => {
 onHide(() => {
   console.log('App Hide')
 })
+
+
 </script>
 <style lang="scss">
 @import 'static/css/index.scss';
+@import '@/uni_modules/uni-scss/index.scss';
 </style>
